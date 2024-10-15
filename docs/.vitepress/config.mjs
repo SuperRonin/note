993fb56baea367d63dc3a06ed03920598f51cf45
xuexingwei <xuexingwei20@163.com>
2024-10-15 19:23:44 +0800

    fix: icon

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "vitepress";
 export default defineConfig({
   title: "Note📝",
   description: "xuexingwei Note",
-  base: "/note/build",
+  base: "/note/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
